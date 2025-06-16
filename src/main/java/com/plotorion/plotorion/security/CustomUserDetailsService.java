cf97757bb19fca6e0183d9c315c0a34e5d2b2300
Andreea Zola <andreeazolandkovschi@gmail.com>
2025-06-16 20:03:28 +0300

    feat: implement CustomUserDetailsService to load user by username

---
+++ src/main/java/com/plotorion/plotorion/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.plotorion.plotorion.security;
+
+
+import com.plotorion.plotorion.model.User;
+import com.plotorion.plotorion.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail,usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("Incorrect username/email or password."));
+        return new CustomUserDetails(user);
+    }
+}
